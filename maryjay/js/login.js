bc5323335cc0a61b815ff72f07e2ab4f74ec30ee
drewfp <50605448+drewfp@users.noreply.github.com>
2022-10-28 19:40:01 -0300

    Add files via upload
    RealFinalVersion.

--- maryjay/js/login.js
+++ maryjay/js/login.js
@@ -5,14 +5,17 @@ botaocad.onclick = function(){
 }
 
 function login(){
-    var email=["larissa@gmail.com","karina@gmail.com"]
-    var senha=["bolsonaro","lula"]
+    var email=["maryjay@gmail.com","karina@gmail.com"]
+    var senha=["123","1234"]
     for(var i=0; i<email.length; i++){
         if (email[i] == document.getElementById("email_login").value && senha[i] == document.getElementById("senha_login").value){
             alert("Seja bem Vindo(a)")
             document.getElementById("email_login").value=""
             document.getElementById("senha_login").value=""
-            window.open("index.html")
+            window.open("produtos.html")
+        }
+        else{
+            alert("Senha invalida, tente novamente!")
         }
     }
 }
